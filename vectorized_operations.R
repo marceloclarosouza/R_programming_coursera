880e16737009596ae56dfb474bc4ed80a8f53216
Marcelo Claro de Souza <marcelo.claro.souza@gmail.com>
2020-02-04 12:26:46 -0300

    vector and matrix operations

---
+++ vectorized_operations.R
@@ -0,0 +1,14 @@
+## vector operations
+x = 1:4 ; y = 6:9
+x +y
+x >2
+x >=2
+y ==8
+x*y
+x/y
+
+## matrix operations
+x = matrix(1:4, 2,2); y = matrix(rep(10,4), 2,2)
+x*y # element-wise multiplication
+x/y
+x %*% y#true matrix multiplication
